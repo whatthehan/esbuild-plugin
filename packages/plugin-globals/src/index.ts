5faf208396419f17f0c142db878914d986ae5b61
whatthehan <592029487@qq.com>
2021-11-22 22:12:24 +0800

    feat: husky

--- packages/plugin-globals/src/index.ts
+++ packages/plugin-globals/src/index.ts
@@ -1,33 +1,36 @@
 import { PluginBuild } from 'esbuild';
 
 export interface GlobalsOptions {
-  [key: string] : string
+  [key: string]: string;
 }
 
 function GlobalsPlugin(options: GlobalsOptions) {
   return {
-    name: "plugin-globals",
+    name: 'plugin-globals',
     setup: (build: PluginBuild) => {
       const regexp = Object.keys(options)
-      .map((key) => `^${key}$`)
-      .join('|');
+        .map((key) => `^${key}$`)
+        .join('|');
 
-      build.onLoad({
-        filter: new RegExp(regexp),
-        namespace: "file"
-      },(args) => {
-        const value = options[args.path]
-        if(!value) {
-          throw new Error(`unknow global config key: ${args.path}`)
-        }
+      build.onLoad(
+        {
+          filter: new RegExp(regexp),
+          namespace: 'file',
+        },
+        (args) => {
+          const value = options[args.path];
+          if (!value) {
+            throw new Error(`unknow global config key: ${args.path}`);
+          }
 
-        return {
-          contents: `export default window['${value}']`,
-          loader: "js"
-        }
-      })
-    }
-  }
+          return {
+            contents: `export default window['${value}']`,
+            loader: 'js',
+          };
+        },
+      );
+    },
+  };
 }
 
-export default GlobalsPlugin
+export default GlobalsPlugin;
